a56dc0e5610d048fee1e5f09083430157776b57f
Toky <rtokynyaina@outlook.com>
2019-05-07 20:42:54 +0300

    initial commit

--- cssIlaina/concat.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
- 
-gulp.task('default', function () {
-  return gulp.src('*.css')
-    .pipe(concatCss("styles/bundle.css"))
-    .pipe(gulp.dest('out/'));
-});
